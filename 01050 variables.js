bc082dbcb02bb2aeb830b7091f446f9e3486767c
Jafet <jafetzamudio5@gmail.com>
2022-06-19 10:30:01 -0700

    Creado

---
+++ 01050 variables.js
@@ -0,0 +1,8 @@
+"use strict";
+
+let greeting = "Hola";
+let name = "Jafet";
+
+console.log(process.argv[2]);
+
+console.log(`${greeting} my name is ${name}`);
---
+++ 01080 arithmetics.js
@@ -0,0 +1,5 @@
+let num = 2;
+let num1 = 4;
+num1--
+let result = num + num1;
+console.log(result)
